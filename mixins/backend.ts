33129889316dd44e05acfc8902863ac3a98782f3
Yahook <adultinter@gmail.com>
2025-10-25 14:28:32 +0200

    Fix duplicated _host in backend

--- mixins/backend.ts
+++ mixins/backend.ts
@@ -10,8 +10,7 @@ export class ZilPayBackend {
   private _host = 'https://api.zilpay.io';
   // private _host = 'http://127.0.0.1:3000';
   //private _host = typeof window !== 'undefined' ?
-    //'https://api.zilpay.io' : `http://127.0.0.1:4000`;
-  private _host = 'https://api.zilpay.io';
+    //'https://api.zilpay.io' : `http://127.0.0.1:4000`;  
   private _api = `api/v1`;
 
   public async getListedTokens(): Promise<ListedTokenResponse> {
